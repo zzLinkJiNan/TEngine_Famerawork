1241fef301bc5271527d56eef10cd61e85921e0d
zz01606811@alibaba-inc.com <zz01606811@alibaba-inc.com>
2023-01-18 17:52:48 +0800

    zz : 操刀UI框架第三天

--- Assets/TResources/Scripts/UIScripts/Scene/Scene_Test.cs
+++ Assets/TResources/Scripts/UIScripts/Scene/Scene_Test.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TEngine.Runtime;
+using DG.Tweening;
 public class Scene_Test : ZZUISceneBase
 {
-    public override void Ini()
-    {
-        base.Ini();
-    }
 
     public override void Iniparameter()
     {
-        base.Iniparameter();
-        foreach (var item in objs)
-        {
-            TLogger.LogInfo(item.ToString());
-        }     
+        if(objs.Length>0){
+            skinTr.SearchGet<Transform>("");
+        }
+    }
+
+    //初始化配置
+    public override void IniDeploy()
+    {
+        maskIsOn = false; //遮罩是否打开
+        maskColor = new Color(0,0,0,0); //遮罩颜色 RGBA : 0~1
+        base.IniDeploy();
+    }
+
+    //添加事件
+    public override void OnAddEvent()
+    {
+         
+    }
+
+     
+
+    //组件赋值
+    public override void SetModles()
+    {
+        
+    }
+
+    //面板被切换后
+    public override void OnChangeScene()
+    {
+        base.OnChangeScene();
+    }
+
+    //UI完成显示后
+    public override void OnShowed()
+    {
+        base.OnShowed();
+    }
+
+    public override void OnUpdateUI()
+    {
+        base.OnUpdateUI();
+
     }
 }
